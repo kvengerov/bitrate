e2e76527e35ab816157f0775e86da80305f8b356
Konstantin <kvengerov@gmail.com>
2018-04-24 22:43:14 +0300

    style navbar, routing home & collection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './routing/app.routing.module';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { CollectionComponent } from './component/collection/collection.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { UploadComponent } from './component/upload/upload.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CollectionComponent
+    CollectionComponent,
+    ProfileComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routing/app.routing.module.ts
+++ src/app/routing/app.routing.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CollectionComponent } from './../component/collection/collection.component';
+import { CollectionComponent } from '../component/collection/collection.component';
+import { ProfileComponent } from '../component/profile/profile.component';
+import { UploadComponent } from '../component/upload/upload.component';
 
 const routes: Routes = [
-
   {
     path: 'collection',
     component: CollectionComponent
-  }
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'upload',
+    component: UploadComponent
+  },
+
 ];
 
 @NgModule({
