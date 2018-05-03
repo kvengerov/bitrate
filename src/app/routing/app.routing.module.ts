3516fde610372757843b710cfdcf04291368a995
kvengerov <kvengerov@gmail.com>
2018-05-03 15:48:05 +0300

    add popular-list

--- src/app/routing/app.routing.module.ts
+++ src/app/routing/app.routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { CollectionComponent } from '../component/collection/collection.component';
 import { ProfileComponent } from '../component/profile/profile.component';
 import { UploadComponent } from '../component/upload/upload.component';
-import { PopularComponent } from '../component/popular/popular.component';
 import {PopularListComponent} from '../component/popular/popular-list/popular-list.component';
 
 const routes: Routes = [
